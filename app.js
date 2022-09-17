5098b0977f82383948d39bda4ad5a3cb71708dbe
Muhammad Zeeshan <muhamadzeeshan03@gmail.com>
2022-09-17 13:30:23 +0500

    upto-video-70

--- app.js
+++ app.js
@@ -3,26 +3,17 @@ const bodyParser= require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use('/add-product',(req, res, next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name= "title"> <button type=""submit>Add Product</button> </input> </form>')
-   
-    }) ;
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
 
 
-
-    
-    app.use('/product',(req, res, next)=>{
-        console.log(req.body)
-        res.redirect('/')
-        }) ;
-        
-app.use('/',(req, res, next)=>{
-
-    res.send('<h1>Hello from Express</h1>')
-    }) ;
-    
-
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not Found</h1>')
+})
 
 app.listen(3000)
