532055a6ac2f36f2c7d0a16e074736c2ae5d82fc
Muhammad Zeeshan <muhamadzeeshan03@gmail.com>
2022-09-18 21:39:34 +0500

    some-ui-added

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,23 @@
+const path = require('path');
+
 const express = require('express');
 
-const routers = express.Router();
+const rootDir = require('../util/path');
 
-routers.get('/add-product',(req, res, next)=>{
+const router = express.Router();
 
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name= "title"> <button type=""submit>Add Product</button> </input> </form>'
-    
-    )}) ;
-    routers.post('/add-product',(req, res, next)=>{
-        console.log(req.body)
-        res.redirect('/')
-    }) ;
+const products = [];
 
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
 
-module.exports =routers;
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
