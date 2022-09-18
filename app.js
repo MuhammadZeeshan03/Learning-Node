651a78260629e5f5ac580d464eead2cd5d277c02
Muhammad Zeeshan <muhamadzeeshan03@gmail.com>
2022-09-19 01:32:01 +0500

    Connected with the Database (SQL-148-Videos-Done)

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
-const adminData = require('./routes/admin')
-const shopData = require('./routes/shop')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'public')));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin',adminData.routes);
-app.use(shopData);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
-})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.listen(3000)
+app.use(errorController.get404);
+
+app.listen(3000);
